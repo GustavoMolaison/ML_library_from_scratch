9fc4cd0361f46d137e3e04f5493e0e563186393f
GustavoMolaison <hubertwolowiec1102@gmail.com>
2025-02-15 11:50:03 +0100

    sd

---
+++ uno.py
@@ -0,0 +1,44 @@
+import numpy as np
+from numpy import ndarray
+
+input_1d = np.array([1,2,3,4,5])
+param_1d = np.array([1,1,1])
+
+
+def _pad_ld(inp: ndarray, num: int) -> ndarray:
+    # add 0 times num to begging and end of an array 
+    z = np.array([0])
+    z = np.repeat(z, num)
+    return np.concatenate([z, inp, z])
+
+
+# inp = input param = filter
+def conv_ld(inp: ndarray, param: ndarray) -> ndarray:
+    
+    
+    # filling entry data
+    param_len = param.shape[0]
+    param_mid = param_len // 2
+    input_pad = _pad_ld(inp, param_mid)
+
+    # initilization of entry data
+    out = np.zeros(inp.shape)
+    print(f'out.shape[0]{out.shape[0]}')
+    print(f'out{out}')
+    print(f'param_len{param_len}')
+    print(f'param_mid{param_mid}')
+    print(f'input_pad{input_pad}')
+    # convulsion of one-dimension data
+    for o in range(out.shape[0]):
+        jump = 0
+        for p in range(param_len):
+            out[o] += param[p] * input_pad[o+p + jump]
+            jump = 1
+        print('coool')
+
+
+    
+    return out
+
+x  = conv_ld(input_1d, param_1d)
+print(x)
