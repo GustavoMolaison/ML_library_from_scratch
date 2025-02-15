40863b87e4378d6c7408d43b7c3ad505bc8ce68a
GustavoMolaison <hubertwolowiec1102@gmail.com>
2025-02-15 13:15:40 +0100

    saddasda

--- uno.py
+++ uno.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy import ndarray
 
-input_1d = np.array([1,2,3,4,5])
+input_1d = np.array([1,2,3,4,5,6,7])
 param_1d = np.array([1,1,1])
 
 
@@ -13,27 +13,34 @@ def _pad_ld(inp: ndarray, num: int) -> ndarray:
 
 
 # inp = input param = filter
-def conv_ld(inp: ndarray, param: ndarray) -> ndarray:
+def conv_ld(inp: ndarray, param: ndarray, jump: int = 1) -> ndarray:
     
     
     # filling entry data
     param_len = param.shape[0]
     param_mid = param_len // 2
-    input_pad = _pad_ld(inp, param_mid)
+    amount_of_param_passes = inp.shape[0] - (param_len - 1)
+    confirmed_iteration =  (1 + ((amount_of_param_passes - 1) // (jump + 1)) )
+    skipped_iteration =  amount_of_param_passes - confirmed_iteration
+    input_pad = _pad_ld(inp, param_len * skipped_iteration)
 
     # initilization of entry data
+
     out = np.zeros(inp.shape)
+    print(f'skipped_iteration{skipped_iteration}')
     print(f'out.shape[0]{out.shape[0]}')
     print(f'out{out}')
     print(f'param_len{param_len}')
     print(f'param_mid{param_mid}')
     print(f'input_pad{input_pad}')
     # convulsion of one-dimension data
+    jump_calc = 0
     for o in range(out.shape[0]):
-        jump = 0
         for p in range(param_len):
-            out[o] += param[p] * input_pad[o+p + jump]
-            jump = 1
+            out[o] += param[p] * input_pad[o+p + jump_calc]
+            
+        jump_calc += jump    
+        print(out)
         print('coool')
 
 
