859b250495288779d77e92f63120a0f803defb46
GustavoMolaison <hubertwolowiec1102@gmail.com>
2025-02-16 23:11:26 +0100

    cooooool deriative classic

--- uno.py
+++ uno.py
@@ -1,8 +1,41 @@
 import numpy as np
 from numpy import ndarray
 
-input_1d = np.array([1,2,3,4,5])
-param_1d = np.array([1,1,1])
+class convenctional_model():
+    def __init__(self):
+        
+        pass
+
+    def initilialization(self,  inp: ndarray, param: ndarray, jump: int = 0 ):
+        self.inp = inp
+        self.param = param
+        self.jump = jump
+
+    class conv_layer():
+        def __init__(self, inp: ndarray, param: ndarray, jump: int = 0 ):
+            self.inp = inp
+            self.param = param
+            self.jump = jump
+            # creating empty data we only wont shapes here altghou function needs actual arrays
+            inp_shape = np.zeros(self.inp.shape)
+            param_shape = np.zeros(param.shape)
+            # getting data after adding 0
+            input_pad = input_pad_calc(inp_shape, param_shape)
+            
+            self.kernels = get_kernels(param_shape, input_pad)
+            self.kernels_weights = np.ones((self.kernels.shape[0], param.shape[0]))
+
+        def get_info(self):
+            print(f'self.kernels{self.kernels.shape}')
+            print(f'self.kernels_weights{self.kernels_weights.shape}')
+
+    def forward(self, input, conv_layer):
+        return conv_ld(input, conv_layer.param, conv_layer.jump)
+
+
+
+
+
 
 
 def _pad_ld(inp: ndarray, num: int) -> ndarray:
@@ -12,14 +45,13 @@ def _pad_ld(inp: ndarray, num: int) -> ndarray:
     
 
     z = np.repeat(z, num)
+
     return np.concatenate([z, inp, z])
 
 
 # inp = input param = filter
-def conv_ld(inp: ndarray, param: ndarray, jump: int = 0) -> ndarray:
-    
-    
-    # filling entry data
+def input_pad_calc(inp: ndarray, param: ndarray, jump: int = 0) -> ndarray:
+     # filling entry data
     param_len = param.shape[0]
     param_mid = param_len // 2
  
@@ -33,8 +65,27 @@ def conv_ld(inp: ndarray, param: ndarray, jump: int = 0) -> ndarray:
  
        input_pad = _pad_ld(input_pad, (skipped_iteration + (jump - 1)))
 
-    # initilization of entry data
+    return input_pad
 
+def kernel_forward(inp: ndarray, param: ndarray, input_pad: ndarray, jump: int = 0) -> ndarray:
+    jump_calc = 0
+    for o in range(inp.shape[0]):
+        for p in range(param.shape[0]):
+            inp[o] += param[p] * input_pad[o+p + jump_calc]
+            
+        jump_calc += jump    
+      
+
+
+    
+    return inp, input_pad
+
+def conv_ld(inp: ndarray, param: ndarray, jump: int = 0) -> ndarray:
+    
+    
+    
+    # initilization of entry data
+    input_pad = input_pad_calc(inp, param, jump)
     out = np.zeros(inp.shape)
     # print(f'skipped_iteration{skipped_iteration}')
     # print(f'out.shape[0]{out.shape[0]}')
@@ -45,22 +96,47 @@ def conv_ld(inp: ndarray, param: ndarray, jump: int = 0) -> ndarray:
     # convulsion of one-dimension data
     jump_calc = 0
     for o in range(out.shape[0]):
-        for p in range(param_len):
+        for p in range(param.shape[0]):
             out[o] += param[p] * input_pad[o+p + jump_calc]
             
         jump_calc += jump    
-        print(out)
-        print('coool')
+      
 
 
     
-    return out
+    return out, input_pad
 
 
 def conv_ld_sum(inp: ndarray, param: ndarray) -> ndarray:
 
-    out = conv_ld(inp, param)
+    out, input_pad = conv_ld(inp, param)
+    
     return np.sum(out)
 
-x  = conv_ld_sum(input_1d, param_1d)
-print(x)
+def get_kernels(param: ndarray, input_pad: ndarray) -> ndarray:
+    kernels = np.zeros((input_pad.shape[0] - (param.shape[0] - 1), param.shape[0]))
+    for i in range(input_pad.shape[0] - (param.shape[0] - 1)):
+        kernels[i] = input_pad[i : param.shape[0] + i]
+    return kernels
+
+def deriative_input(inp: ndarray, param: ndarray, input_pad: ndarray) -> ndarray:
+    
+    kernels = get_kernels(param, input_pad)
+    
+    # for kernel in kernels:
+
+    print(kernels)
+
+    # for i in input_pad:
+
+input_1d = np.array([1,2,3,4,5])
+param_1d = np.array([1,1,1])
+
+# input, pad_inp = conv_ld(input_1d, param_1d)
+# deriative_input(input, param_1d, pad_inp)
+model = convenctional_model()
+conv_1 = model.conv_layer(inp = input_1d, param = param_1d, jump = 0)
+output, pad_input = model.forward(input_1d, conv_1)
+print(output)
+# x  = conv_ld_sum(input_1d, param_1d)
+# print(x)
